047d21dc7154625beab491731549a7ad30e53832
= <=>
2021-02-27 10:14:18 -0300

    aplicação completa

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,18 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
-            <img src="https://github.com/gian-lucas.png" alt="Gian"/>
+            <img src="https://github.com/gian-lucas.png" alt="Gian" />
             <div>
                 <strong>Gian Lucas</strong>
                 <p>
-                    <img src="icons/level.svg" alt="Level"/>
-                    Level 2
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
                 </p>
             </div>
         </div>
